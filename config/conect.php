22488e882e1092979a0d8b0b91c971865e69577e
Chaip <chaiyapornk@nu.ac.th>
2021-05-06 21:15:19 +0700

    first commit

---
+++ config/conect.php
@@ -0,0 +1,8 @@
+<?php
+$host='localhost';
+$user='root';
+$pass='1234';
+$db='db';
+mysqli_connect($host,$user,$db);
+echo "connect success";
+?>
---
+++ config/conect_sample.php
@@ -0,0 +1,8 @@
+<?php
+$host='localhost'; //your host
+$user='root'; //your username
+$pass='1234'; //your password
+$db='db'; //your db
+mysqli_connect($host,$user,$pass,$db);
+echo "connect success";
+?>
